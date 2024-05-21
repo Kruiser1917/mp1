5dc23cf66112afe708df2740c0315c71d7c9916c
aptik <aptik@DESKTOP-SVN8V0A>
2024-05-21 14:43:22 +0300

    обавлена отладочная информация в тест test_universal_masking для диагностики ошибки форматирования

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,8 +1,11 @@
 from src.widget import universal_masking, format_datetime_to_date
 
+
 def test_format_datetime_to_date():
     assert format_datetime_to_date('2018-07-11T02:26:18.671407') == '11.07.2018'
 
 def test_universal_masking():
-    assert universal_masking('Visa Platinum 1234567812345678') == 'Visa Platinum 1234 56** **** 5678'
+    result = universal_masking('Visa Platinum 1234567812345678')
+    print(f"Result: '{result}'")
+    assert result == 'Visa Platinum 1234 56** **** 5678'
     assert universal_masking('Счет 12345678') == 'Счет **5678'
