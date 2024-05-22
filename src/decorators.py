a2f26ecb6fd85657f7c62b96c354d6549acc372d
Rafael <123555722+Kruiser1917@users.noreply.github.com>
2024-05-22 14:58:05 +0300

    Revert "справление декоратора log и тестов. обавлен импорт sys для корректного логирования ошибок, исправлены тесты для проверки вывода в stdout и stderr."
    This reverts commit a9b17c6fca4c0c08d235e6a333ffa1d72714d97b.

--- src/decorators.py
+++ src/decorators.py
@@ -1,8 +1,5 @@
-# src/decorators.py
-
 import functools
 import logging
-import sys  # Добавляем импорт sys
 from typing import Callable, Any, Optional
 
 
@@ -16,32 +13,20 @@ def log(filename: Optional[str] = None) -> Callable:
     Returns:
         Callable: Декорированная функция.
     """
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-
     if filename:
-        handler = logging.FileHandler(filename)
+        logging.basicConfig(filename=filename, level=logging.INFO)
     else:
-        handler = logging.StreamHandler()
-
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(message)s')
-    handler.setFormatter(formatter)
-    if logger.hasHandlers():
-        logger.handlers.clear()
-    logger.addHandler(handler)
+        logging.basicConfig(level=logging.INFO)
 
     def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
         def wrapper(*args, **kwargs) -> Any:
             try:
                 result = func(*args, **kwargs)
-                print(f"{func.__name__} ok")  # Печатаем в stdout
-                logger.info(f"{func.__name__} ok")
+                logging.info(f"{func.__name__} ok")
                 return result
             except Exception as e:
-                print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}", file=sys.stderr)  # Печатаем в stderr
-                logger.error(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                logging.error(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
                 raise
 
         return wrapper
