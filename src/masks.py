2803b79091999f8b230f46692f22d8cab97c58e6
aptik <aptik@DESKTOP-SVN8V0A>
2024-05-22 14:39:08 +0300

    Исправлена функция по маскировке счета.

--- src/masks.py
+++ src/masks.py
@@ -1,23 +1,23 @@
 # маскировки номеров карт и счетов
 
 
+# src/masks.py
+
 def mask_card_number(card_number: str) -> str:
     """
-    Маскирует номер кредитной карты, показывая только первые "
-    "6 и последние 4 цифры. "
+    Маскирует номер кредитной карты, показывая только первые 6 и последние 4 цифры.
 
-    Аргументы:
-    card_number (str): Номер кредитной карты, который будет замаскирован.
+    Args:
+    card_number (str): Номер кредитной карты для маскировки.
 
-    Возвращает:
-    str: Замаскированный номер кредитной карты "
-    "в формате 'XXXX XX** **** XXXX'.
+    Returns:
+    str: Замаскированный номер кредитной карты в формате 'XXXX XX** **** XXXX'.
     """
-    # Проверяем, что номер карты состоит только из цифр и имеет допустимую длин
-    if 13 <= len(card_number) <= 19 and card_number.isdigit():
+    if len(card_number) == 16 and card_number.isdigit():
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     else:
-        raise ValueError("Неверный номер карты. Номер карты должен содержать от 13 до 19 цифр.")
+        raise ValueError("Invalid card number. Card number must have exactly 16 digits.")
+
 
 
 def mask_account_number(account_number: str) -> str:
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,11 +1,13 @@
-from src.widget import universal_masking, format_datetime_to_date
+# tests/test_widget.py
 
+import pytest
+from src.widget import universal_masking
+from src.widget import format_datetime_to_date
 
 def test_format_datetime_to_date():
-    assert format_datetime_to_date('2018-07-11T02:26:18.671407') == '11.07.2018'
+    assert format_datetime_to_date("2018-07-11T02:26:18.671407") == "11.07.2018"
 
 def test_universal_masking():
     result = universal_masking('Visa Platinum 1234567812345678')
     print(f"Result: '{result}'")
     assert result == 'Visa Platinum 1234 56** **** 5678'
-    assert universal_masking('Счет 12345678') == 'Счет **5678'
