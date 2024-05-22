ac6800830a6b4fcb8b5849f117995113cc56e793
aptik <aptik@DESKTOP-SVN8V0A>
2024-05-22 13:56:00 +0300

    обавлен декоратор log для логирования вызовов функций и результатов их работы

---
+++ tests/test_decorators.py
@@ -0,0 +1,51 @@
+import pytest
+
+from src.decorators import log
+
+
+# Пример функции для тестирования
+@log()
+def add(x, y):
+    return x + y
+
+
+@log()
+def raise_error():
+    raise ValueError("Test error")
+
+
+def test_add(capsys):
+    result = add(1, 2)
+    assert result == 3
+    captured = capsys.readouterr()
+    assert "add ok" in captured.out or "add ok" in captured.err
+
+
+def test_raise_error(capsys):
+    with pytest.raises(ValueError):
+        raise_error()
+    captured = capsys.readouterr()
+    assert "raise_error error: Test error" in captured.out or "raise_error error: Test error" in captured.err
+
+
+def test_log_to_file(tmpdir):
+    log_file = tmpdir.join("test_log.txt")
+
+    @log(filename=str(log_file))
+    def add_to_file(x, y):
+        return x + y
+
+    add_to_file(1, 2)
+    with open(log_file, 'r') as file:
+        logs = file.read()
+        assert "add_to_file ok" in logs
+
+    @log(filename=str(log_file))
+    def raise_error_to_file():
+        raise ValueError("Test error to file")
+
+    with pytest.raises(ValueError):
+        raise_error_to_file()
+    with open(log_file, 'r') as file:
+        logs = file.read()
+        assert "raise_error_to_file error: Test error to file" in logs
